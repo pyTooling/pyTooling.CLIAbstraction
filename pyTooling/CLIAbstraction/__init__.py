ac390e68c54a8a71343e4d840b6ecdd8ad8d66d9
Patrick Lehmann <Paebbels@gmail.com>
2022-03-02 11:56:13 +0100

    Bumped version to v0.4.1.

--- pyTooling/CLIAbstraction/__init__.py
+++ pyTooling/CLIAbstraction/__init__.py
@@ -34,7 +34,7 @@ __author__ =    "Patrick Lehmann"
 __email__ =     "Paebbels@gmail.com"
 __copyright__ = "2014-2022, Patrick Lehmann"
 __license__ =   "Apache License, Version 2.0"
-__version__ =   "0.4.0"
+__version__ =   "0.4.1"
 __keywords__ =  ["abstract", "executable", "cli", "cli arguments"]
 
 from pathlib              import Path
